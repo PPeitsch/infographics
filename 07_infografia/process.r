4bdca2fa65e9d8c4a88331606e88037b113ae981
PPeitsch <pablo_peitsch@pablo.peitsch>
2022-10-17 04:50:44 -0300

    finishing

--- 07_infografia/process.r
+++ 07_infografia/process.r
@@ -4,6 +4,8 @@ library(ggridges)
 library(reshape2)
 library(gridExtra)
 
+fig_folder <- "fig"
+
 #flights <- flights %>% filter_if(~is.numeric(.), all_vars(!is.infinite(.)))
 flights <- flights %>% filter_all(all_vars(!is.infinite(.)))
 #flights <- flights[!is.infinite(rowSums(flights)),]
@@ -32,7 +34,7 @@ p1 <- ggplot(data=flights_grouped_carrier,
         ) +
     scale_x_discrete('Aerolínea', label=carriers_) +
     theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 25))
-
+fig1_name <- "fig1.jpeg"
 
 flights_melted_3 <- melt(flights_grouped, id=c("carrier", "month"))
 flights_grouped_ <- group_by(flights_melted_3, month)
@@ -48,9 +50,10 @@ p2 <- ggplot(data=flights_grouped_, aes(y=factor(carrier), x=value, color=factor
     scale_x_continuous(expand = c(0, 0)) +
     scale_y_discrete(expand = c(0, 0), 'Aerolínea', label=carriers_) +
     theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 25))
+fig2_name <- "fig2.jpeg"
 
 
-flights_reduced <- filter(flights_grouped, carrier==c('OO', 'YV', 'HA', 'FL', 'F9', 'AS', '9E'))
+flights_reduced <- filter(flights_grouped, carrier==c('OO', 'HA', 'FL', 'F9', 'AS', '9E'))
 #flights_reduced <- filter(flights_melted_3, carrier==c('OO', 'YV', 'HA', 'FL', 'F9', 'AS', '9E'))
 p3 <- ggplot(data=flights_reduced) +
         geom_boxplot(aes(y=departure, x=factor(carrier), color=factor(month))) +
@@ -64,8 +67,9 @@ p4 <- ggplot(data=flights_reduced) +
         coord_cartesian(ylim=c(-1,2.5)) +
         labs(y='Retraso de arribo (min)', color = 'Etiqueta') +
         scale_color_discrete(label=months_) +
-        scale_x_discrete('Aerolínea', label=c('OO', 'YV', 'HA', 'FL', 'F9', 'AS', '9E')) +
+        scale_x_discrete('Aerolínea', label=c('OO', 'HA', 'FL', 'F9', 'AS', '9E')) +
         theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 25))
+fig3_name <- "fig3.jpeg"
 
 
 flights_melted_4 <- flights_grouped[complete.cases(flights_grouped), ]
@@ -81,4 +85,11 @@ flights_metricas <- filter(flights_melted_4, carrier==c('FL')) %>% group_by(mont
                                 #iqr_dep_delay=iqr(dep_delay),
                                 )# %>% filter(carrier==c('AS', 'FL', 'YV')) %>% melt(id=c("carrier", "month"))
 
-uu <- melt(flights_metricas, id=c("carrier", "month"))
+flights_grouped2 <- summarise(flights, .groups="month", month, departure=dep_delay/60, arrival=arr_delay/60, carrier, origin,dest)
+flights_reduced2 <- filter(flights_grouped2, carrier==c('OO', 'HA', 'FL', 'F9', 'AS', '9E'))
+flights_OO <- filter(flights_reduced2, carrier==c('OO'))
+flights_HA <- filter(flights_reduced2, carrier==c('HA'))
+flights_FL <- filter(flights_reduced2, carrier==c('FL'))
+flights_F9 <- filter(flights_reduced2, carrier==c('F9'))
+flights_AS <- filter(flights_reduced2, carrier==c('AS'))
+flights_9E <- filter(flights_reduced2, carrier==c('9E'))
