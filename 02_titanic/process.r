d7c92dcbf2ccdafc916ab64129b882102d39b078
Pablo <Pablo Peitsch>
2023-04-03 21:52:11 -0300

    updating basic analysis of Titanic dataset

--- 02_titanic/process.r
+++ 02_titanic/process.r
@@ -1,7 +1,6 @@
 # Library imports
 library(tidyverse)
 
-
 # Datasets folder
 data_folder <- "data"
 
@@ -28,14 +27,33 @@ variables[4] <- "parents"
 variables[6] <- "port"
 colnames(df_titanic) <- variables
 
+# Summary of passengers and survivors
+df_titanic_st <- group_by(df_titanic, survived, class) %>%
+  summarize(n_pasenger = n(),
+            mean_age = mean(age, na.rm = TRUE))
+
+# Graphic of survivors
+ggplot(data = df_titanic_st, aes(x = class, y = n_pasenger, fill = survived)) +
+  geom_bar(stat = "identity", color = "white", alpha = 0.8) +
+  theme_minimal() +
+  labs(x = 'Categoría de pasaje', y = 'Cantidad de personas', title = "Estadística breve de sobrevivientes del Titanic") +
+  theme(legend.position = "right")
+
+
 # Sorting dataset by age
-df_titanic_st <- arrange(df_titanic, age)
+#df_titanic_st <- arrange(df_titanic, age)
 
 # Statistics of dataset
-min_ticket <- min(df_titanic$fare)
-max_ticket <- max(df_titanic$fare)
-mean_ticket <- mean(df_titanic$fare)
+#min_ticket <- min(df_titanic$fare)
+#max_ticket <- max(df_titanic$fare)
+#mean_ticket <- mean(df_titanic$fare)
 
+#min_age <- min(df_titanic$age)
+#max_age <- max(df_titanic$age)
+#mean_age <- mean(df_titanic$age)
 
+#df_titanic_st <- summarise(df_titanic, min())
 
-#df_titanic-st <- summarise(df_titanic, min())
+  #survivors = count(survived == 'yes')/n_pasenger * 100,
+  #gender = mean(sex == "male") * 100,
+  #first_class = count(class == 'yes')/n_pasenger * 100,
