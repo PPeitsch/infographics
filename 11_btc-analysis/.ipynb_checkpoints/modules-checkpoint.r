b26f1f56f8010135efd81e9ef387659d09f864b2
PPeitsch <pablo.peitsch@gmail.com>
2023-07-03 01:35:58 -0300

    changes in regression models

--- 11_btc-analysis/.ipynb_checkpoints/modules-checkpoint.r
+++ 11_btc-analysis/.ipynb_checkpoints/modules-checkpoint.r
@@ -4,7 +4,7 @@ source("core.r")
 # Bitcoin vs time
 g1 <- ggplot(combined_data) + 
         geom_line(aes(x = Date, y = Close_btc), color = 'Red', size = 1.5) +
-        labs(x = "Fecha", y = "Precio BTC [USD]", title = "Precio del Bitcoin, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Precio BTC (usd)", title = "Precio del Bitcoin, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g1_name <- "g1.jpeg"
@@ -12,7 +12,7 @@ g1_name <- "g1.jpeg"
 # SP500 vs time
 g2 <- ggplot(combined_data) + 
         geom_line(aes(x = Date, y = Close_sp500), color = 'Blue', size = 1.5) +
-        labs(x = "Fecha", y = "Precio SP500 [USD]", title = "Precio del Indice SP500, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Precio SP500 (usd)", title = "Precio del Indice SP500, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g2_name <- "g2.jpeg"
@@ -20,7 +20,7 @@ g2_name <- "g2.jpeg"
 # Nasdaq vs time
 g3 <- ggplot(combined_data) + 
         geom_line(aes(x = Date, y = Close_nasdaq), color = 'Green', size = 1.5) +
-        labs(x = "Fecha", y = "Precio Nasdaq [USD]", title = "Precio del Nasdaq, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Precio Nasdaq (usd)", title = "Precio del Nasdaq, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g3_name <- "g3.jpeg"
@@ -28,7 +28,7 @@ g3_name <- "g3.jpeg"
 # Founds Rate vs time
 g4 <- ggplot(combined_data_fed) +
         geom_line(aes(x = DATE, y = tasa_FED), color = 'Orange', size = 1.5) +
-        labs(x = "Fecha", y = "Tasa de interes", title = "Tasa de interes de referencia en EEUU, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Tasa de interes", title = "Tasa de interes de referencia en EEUU, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g4_name <- "g4.jpeg"
@@ -36,7 +36,7 @@ g4_name <- "g4.jpeg"
 # GDP vs time
 g5 <- ggplot(combined_data_fed) +
         geom_line(aes(x = DATE, y = PBI_FED), color = 'Black', size = 1.5) +
-        labs(x = "Fecha", y = "Producto Bruto Interno", title = "Producto Bruto Interno de EEUU, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Producto Bruto Interno", title = "Producto Bruto Interno de EEUU, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g5_name <- "g5.jpeg"
@@ -44,7 +44,7 @@ g5_name <- "g5.jpeg"
 #  vs time
 g6 <- ggplot(combined_data_fed) +
         geom_line(aes(x = DATE, y = desempleo_FED), color = 'Blue', size = 1.5) +
-        labs(x = "Fecha", y = "Tasa de desempleo", title = "Tasa de desempleo en EEUU, periodo 2014-2023") +
+        labs(x = "Fecha (días)", y = "Tasa de desempleo", title = "Tasa de desempleo en EEUU, periodo 2014-2023") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g6_name <- "g6.jpeg"
@@ -53,59 +53,80 @@ g6_name <- "g6.jpeg"
 g7 <- ggplot(combined_data_fed) +
         geom_line(aes(x = DATE, y = tasa_FED), color = 'Red', size = 1.5) +
         geom_line(aes(x = DATE, y = desempleo_FED), color = 'Blue', size = 1.5) +
-        labs(x = "Fecha", y = "Tasa (%)", title = "Datos macroeconomicos de EEUU") +
+        labs(x = "Fecha (días)", y = "Tasa (%)", title = "Datos macroeconomicos de EEUU") +
         theme(axis.line = element_line(colour="black", size=1), text = element_text(size=30),
               plot.title = element_text(color="Black", size=30, face="bold"))
 g7_name <- "g7.jpeg"
 
 
 
-# First model
-m1 <- ggplot(data=df_model_1) + 
-        geom_point(aes(x=Date, y=Close_btc)) +
-        geom_line(data=df_model_1, aes(x=Date, y=pred), color='Blue', size=1.5) +
-        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 30),
-              plot.title = element_text(color="Black", size=30, face="bold")) +
-        labs(x="Fecha (años)", y="Precio (u$s)", title="Regresion lineal: modelo 1")
+# First and second model
+m1 <- ggplot(data = df_model_1_2) +
+        geom_point(aes(x = Close_btc, y = Volume_btc), color = "DarkGray") +
+        geom_line(aes(x = Close_btc, y = pred, color = Model), size = 1.5) +
+        scale_y_continuous(labels = scales::comma_format(), trans = 'log10') +
+        theme(axis.line = element_line(colour = "black", size = 1),
+        text = element_text(size = 30),
+        plot.title = element_text(color = "Black", size = 30, face = "bold")) +
+        labs(x = "Precio (usd)", y = "Volumen (millones de usd)", title = "Regresión lineal: Modelos 1 y 2")
 m1_name <- "m1.jpeg"
-# Residual graph
-m2 <- ggplot(data=df_model_1, aes(y = resid)) +
-        geom_boxplot() +
-        labs(title='Boxplot: Residuals', y='Residuals value') +
-        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 40),
-              plot.title = element_text(color="Black", size=40, face="bold"))
+# First and second model
+m2 <- ggplot(data = df_model_1_2) +
+        geom_point(aes(x = Close_btc, y = Volume_btc), color = "DarkGray", size = 2) +
+        geom_line(aes(x = Close_btc, y = pred, color = Model), size = 1.5) +
+        facet_wrap( ~ Model, nrow = 2) +
+        scale_y_continuous(labels = scales::comma_format(), trans = 'log10') +
+        theme(axis.line = element_line(colour = "black", size = 1),
+        text = element_text(size = 30),
+        plot.title = element_text(color = "Black", size = 30, face = "bold")) +
+        labs(x = "Precio (usd)", y = "Volumen (millones de usd)", title = "Regresión lineal: Modelos 1 y 2")
 m2_name <- "m2.jpeg"
 
-
-# Second model
-m3 <- ggplot(data=df_model_2) + 
-        geom_point(aes(x=Date, y=Close_btc)) +
-        geom_line(data=df_model_2, aes(x=Date, y=pred), color='Blue', size=1.5) +
-        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 30),
-              plot.title = element_text(color="Black", size=30, face="bold")) +
-        labs(x="Fecha (años)", y="Precio (u$s)", title="Regresion lineal: modelo 2")
-m3_name <- "m3.jpeg"
 # Residual graph
-m4 <- ggplot(data=df_model_2, aes(y = resid)) +
+r1 <- ggplot(data=df_model_1, aes(y = resid)) +
         geom_boxplot() +
         labs(title='Boxplot: Residuals', y='Residuals value') +
         theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 40),
               plot.title = element_text(color="Black", size=40, face="bold"))
-m4_name <- "m4.jpeg"
+r1_name <- "m1_residuals.jpeg"
+
+# Residual graph
+r2 <- ggplot(data=df_model_2, aes(y = resid)) +
+        geom_boxplot() +
+        labs(title='Boxplot: Residuals', y='Residuals value') +
+        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 40),
+              plot.title = element_text(color="Black", size=40, face="bold"))
+r2_name <- "m2_residuals.jpeg"
 
 
 # Third model
-m5 <- ggplot(data=df_model_3) + 
+m3 <- ggplot(data=df_model_3) + 
         geom_point(aes(x=Date, y=Close_btc)) +
-        geom_line(data=df_model_3, aes(x=Date, y=pred), color='Blue', size=1.5) +
+        geom_line(data=df_model_3, aes(x=Date, y=pred), color='DarkGreen', size=1.5) +
         theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 30),
               plot.title = element_text(color="Black", size=30, face="bold")) +
-        labs(x="Fecha (años)", y="Precio (u$s)", title="Regresion lineal: modelo 3")
-m5_name <- "m5.jpeg"
+        labs(x="Fecha (días)", y="Precio (usd)", title="Regresion lineal: modelo 3")
+m3_name <- "m3.jpeg"
 # Residual graph
-m6 <- ggplot(data=df_model_3, aes(y = resid)) +
+r3 <- ggplot(data=df_model_3, aes(y = resid)) +
         geom_boxplot() +
         labs(title='Boxplot: Residuals', y='Residuals value') +
         theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 40),
               plot.title = element_text(color="Black", size=40, face="bold"))
-m6_name <- "m6.jpeg"
+r3_name <- "m3_residuals.jpeg"
+
+# Fourth model
+m4 <- ggplot(data = df_model_3_4) +
+        geom_point(aes(x = Date, y = Close_btc), color = "DarkGreen") +
+        geom_line(aes(x = Date, y = pred, color = Model), size = 1) +
+        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 30),
+        plot.title = element_text(color = "Black", size = 30, face = "bold")) +
+        labs(x = "Fecha (días)", y = "Precio (usd)", title = "Regresión lineal: modelos 3 y 4")
+m4_name <- "m4.jpeg"
+# Residual graph
+r4 <- ggplot(data=df_model_4, aes(y = resid)) +
+        geom_boxplot() +
+        labs(title='Boxplot: Residuals', y='Residuals value') +
+        theme(axis.line = element_line(colour = "black", size = 1), text = element_text(size = 40),
+              plot.title = element_text(color="Black", size=40, face="bold"))
+r4_name <- "m4_residuals.jpeg"
--- 11_btc-analysis/core.r
+++ 11_btc-analysis/core.r
@@ -61,20 +61,66 @@ names(combined_data_fed)[names(combined_data_fed) == "FEDFUNDS"] <- "tasa_FED"
 combined_data <- data.frame(combined_data)
 combined_data_fed <- data.frame(combined_data_fed)
 
+combined_data <- combined_data[-1616, ]
+reduced_data <- subset(combined_data, select=c("Date", "Close_btc", "Close_oro", "Close_brent", "Close_wti", "Close_sp500", "Close_nasdaq", "Volume_btc"))
+
+
 
 # First model
-df_model_1 <- subset(combined_data, select=c("Date", "Close_btc", "Close_oro", "Close_brent", "Close_nasdaq"))
-model_1 = lm( Close_btc + Close_oro + Close_brent + Close_nasdaq ~ Date, data=df_model_1)
+df_model_1 <- subset(combined_data, select=c("Date", "Close_btc", "Volume_btc"))
+df_model_1 <- mutate(df_model_1, Volume_btc=Volume_btc/1000000)
+model_1 = lm(Volume_btc ~ Close_btc, data=df_model_1)
 df_model_1 <- add_predictions(data=df_model_1, model=model_1) %>% add_residuals(model=model_1)
 
-
 # Second model
-df_model_2 <- df_model_1
-model_2 = lm(Close_btc ~ Date + Close_oro + Close_brent + Close_nasdaq, data=df_model_2)
+df_model_2 <- subset(combined_data, select=c("Date", "Close_btc", "Volume_btc"))
+df_model_2 <- mutate(df_model_2, Volume_btc=Volume_btc/1000000)
+model_2 = lm( Volume_btc ~ log(Close_btc), data=df_model_2)
 df_model_2 <- add_predictions(data=df_model_2, model=model_2) %>% add_residuals(model=model_2)
+
+# Combining the dataframes
+df_model_1_2 <- rbind(transform(df_model_1, Model = "Modelo 1"), transform(df_model_2, Model = "Modelo 2"))
+
+
 
 
 # Third model
-df_model_3 <- df_model_1
-model_3 = lm(Close_btc ~ Close_oro*Date + Close_brent*Date + Close_nasdaq*Date, data=df_model_3)
+df_model_3 <- reduced_data
+model_3 = lm(Close_btc ~ (Close_oro + Close_wti + Close_nasdaq)*Date, data=df_model_3)
 df_model_3 <- add_predictions(data=df_model_3, model=model_3) %>% add_residuals(model=model_3)
+
+# Fourth model
+df_model_4 <- reduced_data
+model_4 = lm(Close_btc ~ (Close_oro + Close_wti + Close_nasdaq + Volume_btc)*Date, data=df_model_4)
+df_model_4 <- add_predictions(data=df_model_4, model=model_4) %>% add_residuals(model=model_4)
+
+# Combining the dataframes
+df_model_3_4 <- rbind(transform(df_model_3, Model = "Model 3"), transform(df_model_4, Model = "Model 4"))
+
+
+
+
+
+
+
+
+
+# for residuals boxplot graph
+residuals <- inner_join(df_model_1, df_model_2, by=c("Date", "Close_btc")) %>% group_by(Date, Close_btc)
+res <- summarize(residuals, mod1 = pred.x + resid.x, mod2 = pred.y + resid.y)
+######### set of models volume
+df_model_vol_btc <- subset(combined_data, select=c("Date", "Close_btc", "Volume_btc"))
+model_vol_btc = lm(Close_btc ~ Volume_btc, data=df_model_vol_btc)
+df_model_vol_btc <- add_predictions(data=df_model_vol_btc, model=model_vol_btc) %>% add_residuals(model=model_vol_btc)
+
+df_model_vol_oro <- subset(combined_data, select=c("Date", "Close_oro", "Volume_oro"))
+model_vol_oro = lm(Close_oro ~ Volume_oro, data=df_model_vol_oro)
+df_model_vol_oro <- add_predictions(data=df_model_vol_oro, model=model_vol_oro) %>% add_residuals(model=model_vol_oro)
+
+df_model_vol_wti <- subset(combined_data, select=c("Date", "Close_wti", "Volume_wti"))
+model_vol_wti = lm(Close_wti ~ Volume_wti, data=df_model_vol_wti)
+df_model_vol_wti <- add_predictions(data=df_model_vol_wti, model=model_vol_wti) %>% add_residuals(model=model_vol_wti)
+
+df_model_vol_ndq <- subset(combined_data, select=c("Date", "Close_nasdaq", "Volume_nasdaq"))
+model_vol_ndq = lm(Close_nasdaq ~ Volume_nasdaq, data=df_model_vol_ndq)
+df_model_vol_nsq <- add_predictions(data=df_model_vol_ndq, model=model_vol_ndq) %>% add_residuals(model=model_vol_ndq)
